f98d6624e81f915f4de6b6f6b3a12e203b0c7b71
wukwuk <seicrossgg@gmail.com>
2022-06-20 21:47:50 +0800

    ganti nomer 3

--- UAS/soal-no-3.py
+++ UAS/soal-no-3.py
@@ -4,9 +4,11 @@ while True:
     user = input("Masukan Angka : ")
     if user == 'n':
         break
+    x += 1
     data.append(user)
 
-jumlah = 0
+rata2 = 0
 for nilai in data:
-    jumlah += int(nilai)
-print(jumlah)
+    rata2 += int(nilai)
+rata2 /= x
+print(f"Rata - rata nilai : {rata2}")
